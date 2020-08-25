7b7e0ae9f78e3d65bf0d016ba108cb5c98cea5ee
Ravi Kalla <ravi2523096@gmail.com>
2020-08-25 14:34:49 -0400

    Added HTTPS property while getting from DB

--- src/main/java/in/ravikalla/wiremockbackup/repo/InstanceMappingRepository.java
+++ src/main/java/in/ravikalla/wiremockbackup/repo/InstanceMappingRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.mongodb.repository.Query;
 import in.ravikalla.wiremockbackup.document.InstanceMapping;
 
 public interface InstanceMappingRepository extends MongoRepository<InstanceMapping, Long> {
-	@Query(value = "{}", fields = "{ 'id' : 1, 'instanceName' : 1, 'host' : 1, 'port' : 1, 'targetURL' : 1}")
+	@Query(value = "{}", fields = "{ 'id' : 1, 'instanceName' : 1, 'https' : 1, 'host' : 1, 'port' : 1, 'targetURL' : 1}")
 	List<InstanceMapping> findBasicDetails();
 
-	@Query(value = "{'id' : ?0 }", fields = "{ 'id' : 1, 'instanceName' : 1, 'host' : 1, 'port' : 1, 'targetURL' : 1}")
+	@Query(value = "{'id' : ?0 }", fields = "{ 'id' : 1, 'instanceName' : 1, 'https' : 1, 'host' : 1, 'port' : 1, 'targetURL' : 1}")
 	InstanceMapping findBasicDetailsById(Long instanceId);
 
 	@Query(value = "{'id' : ?0 }", fields = "{ 'mappings' : 1}")
